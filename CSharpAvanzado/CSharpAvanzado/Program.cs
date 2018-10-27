ce691590c4da0ae87434563b190199649ab0af47
eduardomedina <emedina@alset.com.mx>
2018-10-26 22:05:06 -0500

    -Let's get started

---
+++ CSharpAvanzado/CSharpAvanzado/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpAvanzado
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("HELLO WORLD");
+        }
+    }
+}
